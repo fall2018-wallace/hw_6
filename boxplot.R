4346e414e42a8c9b5003dcf0826432645c716a48
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 23:34:26 -0400

    Saving boxplot.R

--- boxplot.R
+++ boxplot.R
@@ -7,5 +7,8 @@ box_population=ggplot(merged_data,aes(x=factor(0),y=population))+geom_boxplot()
 
 #box plot of nurder rate
 box_murder=ggplot(merged_data,aes(x=factor(0),y=Murder))+geom_boxplot()
+
 box_population
 box_murder
+
+#Box plots are used when the data variation is less. here, the variation in data is more, 
