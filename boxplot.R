621598c89730fb5dd783134faf5719d92a70b84b
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 23:33:41 -0400

    Saving boxplot.R

--- boxplot.R
+++ boxplot.R
@@ -1,7 +1,11 @@
 
 library(ggplot2)
 options(scipen=999)
+
+#Box plot of population
 box_population=ggplot(merged_data,aes(x=factor(0),y=population))+geom_boxplot()
+
+#box plot of nurder rate
 box_murder=ggplot(merged_data,aes(x=factor(0),y=Murder))+geom_boxplot()
 box_population
 box_murder
