07f154ed7b58249bad2eb13d1b97a079ba395e40
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 21:31:51 -0400

    Saving hist.R

--- hist.R
+++ hist.R
@@ -1,7 +1,10 @@
 
-ggplot(merged_data,aes(x=population))+geom_histogram(bins=50,color="black", fill="black")
-ggplot(merged_data,aes(x=Murder))+geom_histogram(bins=50,color="black", fill="blue")
-ggplot(merged_data,aes(x= Assault))+geom_histogram(bins=50,color="black", fill="black")
-ggplot(merged_data,aes(x=UrbanPop))+geom_histogram(bins=50,color="black", fill="black")
-ggplot(merged_data,aes(x=Rape))+geom_histogram(bins=50,color="black", fill="black")
 
+
+hist_pop=ggplot(merged_data,aes(x=population))+geom_histogram(bins=50,color="black", fill="black")
+hist_murder=ggplot(merged_data,aes(x=Murder))+geom_histogram(bins=50,color="black", fill="blue")
+hist_assault=ggplot(merged_data,aes(x= Assault))+geom_histogram(bins=50,color="black", fill="black")
+hist_urbanpop=ggplot(merged_data,aes(x=UrbanPop))+geom_histogram(bins=50,color="black", fill="black")
+hist_rapeggplot(merged_data,aes(x=Rape))+geom_histogram(bins=50,color="black", fill="black")
+
+hist_pop
