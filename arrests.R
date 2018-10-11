a94bf527693518abc1b7a1582ceb3aee5ca88eaf
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 23:26:55 -0400

    Saving arrests.R

--- arrests.R
+++ arrests.R
@@ -1,7 +1,8 @@
 
-USArrests
+
+USArrests #accesing he built in data frame
 arrests=USArrests
-arrests=cbind(rownames(arrests),arrests)
-colnames(arrests)= c("Statename")
+arrests=cbind(rownames(arrests),arrests) #creating additional column with names of built in rows 
+colnames(arrests)= c("Statename") #Naming the new column
 arrests
 
