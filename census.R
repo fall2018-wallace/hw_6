f6d9df6dfe20552bf311b1f7758cf506a426812b
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 23:25:28 -0400

    Saving census.R

--- census.R
+++ census.R
@@ -1,7 +1,13 @@
 
+##################################################################
+# IST 687- Introduction to Data Science
+# Due date - 10/10/18 by 11:59 p.m.
+# Homework 6:Data Viz- Submitted by Simaant Jayant Patil on 10/10/18
+# Portions of this code came from Introduction to Data Science
+# but the comments are all original.
 
-
-#Reading the csv from a data frame.df_states <- read.csv("scprc_est_2017_18_pop_res.csv")
+#Reading the csv from a data frame.
+df_states <- read.csv("scprc_est_2017_18_pop_res.csv")
 df_states
 
 new1=df_states[,-4]  #new data frame without the "STATE" column
