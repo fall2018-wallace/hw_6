6f3d8c24377dc9a1a77b97c42279a7c5ede4f266
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 21:54:09 -0400

    Saving boxplot.R

--- boxplot.R
+++ boxplot.R
@@ -1,5 +1,6 @@
 
 library(ggplot2)
+options(scipen=999)
 box_population=ggplot(merged_data,aes(x=factor(0),y=population))+geom_boxplot()
 box_murder=ggplot(merged_data,aes(x=factor(0),y=Murder))+geom_boxplot()
 box_population
