9612009d90e7b4b2f5e937b4c33c9c07109e55c1
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 20:09:01 -0400

    moving clean.R to census.R

---
+++ census.R
@@ -0,0 +1,33 @@
+
+#Reading the csv from the web into a data frame.
+
+df_states <- read.csv("scprc_est_2017_18_pop_res.csv")
+df_states
+
+new1=df_states[,-4]  #new data frame without the "STATE" column
+new1          
+ 
+new2=new1[-53,]     #Removing the last row from the new data set
+new2
+
+
+new3=new2[-1,]      #Removing the first row from the new data set
+new3
+
+
+new4=new3[,-1:-3]  #Removing the first three columns,i.e., "SUMLEV", "REGION" and "DIVISION"
+new4 
+
+
+colnames(new4)=c("Statename","population","popOver18","percentOver18") #Changing the names of remaining columns
+new4
+
+
+#Creating a function with no parameters
+
+no_par=function()
+{
+  return(new4)
+}
+census=no_par()
+census
