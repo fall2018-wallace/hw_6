508c9dc8c1a1a63b5c64c14f338def0db6c70d5d
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 20:55:21 -0400

    Saving merged_data.R

--- merged_data.R
+++ merged_data.R
@@ -2,3 +2,8 @@
 merged_data=merge(arrests,census)
 colnames(merged_data)=c("Statename","Murder","Assault","UrbanPop", "Rape","population","popover18", "percentover18")
 merged_data
+
+library(ggplot2)
+
+options(scipen=999)
+
