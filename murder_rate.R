4d0f7648f1ee0e628190c75cb1ec216b458dcf38
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 22:15:41 -0400

    Saving murder_rate.R

--- murder_rate.R
+++ murder_rate.R
@@ -5,3 +5,9 @@ new=merged_data$population/merged_data$Murder
 no_of_murders=new/100000
 no_of_murders
 
+ggplot(merged_data,aes(x=Statename,y=no_of_murders))+geom_col()+theme(axis.text.x=element_text(angle=90, hjust=1))+ggtitle("Total murders")
+ggplot(merged_data,aes(x=reorder(Statename, no_of_murders),y=no_of_murders))+geom_col()+theme(axis.text.x=element_text(angle=90, hjust=1))+ggtitle("Total murders")
+ggplot(merged_data,aes(x=reorder(Statename, no_of_murders),y=no_of_murders))+geom_col(aes(fill=percentover18))+theme(axis.text.x=element_text(angle=90, hjust=1))+ggtitle("Total murders")
+
+
+
