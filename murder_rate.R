045192f25bd2c6fb3665b751fb8d4918266ee3fd
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 22:20:10 -0400

    Saving murder_rate.R

--- murder_rate.R
+++ murder_rate.R
@@ -5,9 +5,10 @@ new=merged_data$population/merged_data$Murder
 no_of_murders=new/100000
 no_of_murders
 
-ggplot(merged_data,aes(x=Statename,y=no_of_murders))+geom_col()+theme(axis.text.x=element_text(angle=90, hjust=1))+ggtitle("Total murders")
-ggplot(merged_data,aes(x=reorder(Statename, no_of_murders),y=no_of_murders))+geom_col()+theme(axis.text.x=element_text(angle=90, hjust=1))+ggtitle("Total murders")
-ggplot(merged_data,aes(x=reorder(Statename, no_of_murders),y=no_of_murders))+geom_col(aes(fill=percentover18))+theme(axis.text.x=element_text(angle=90, hjust=1))+ggtitle("Total murders")
+murders_per_state1=ggplot(merged_data,aes(x=Statename,y=no_of_murders))+geom_col()
+murders_per_state2=ggplot(merged_data,aes(x=Statename,y=no_of_murders))+geom_col()+theme(axis.text.x=element_text(angle=90, hjust=1))+ggtitle("Total murders")
+murders_per_state3=ggplot(merged_data,aes(x=reorder(Statename, no_of_murders),y=no_of_murders))+geom_col()+theme(axis.text.x=element_text(angle=90, hjust=1))+ggtitle("Total murders")
+popover18_state3=ggplot(merged_data,aes(x=reorder(Statename, no_of_murders),y=no_of_murders))+geom_col(aes(fill=percentover18))+theme(axis.text.x=element_text(angle=90, hjust=1))+ggtitle("Total murders")
 
 
 
