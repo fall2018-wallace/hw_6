bf286719ab9143933eb8d3fd02b1a6faff660569
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 21:18:49 -0400

    Saving merged_data.R

--- merged_data.R
+++ merged_data.R
@@ -2,7 +2,7 @@
 merged_data=merge(arrests,census) #merging two datasets from previous two nodes
 
 #renaming the columns of the merged dataset
-colnames(merged_data)=c("Statename","Murder","Assault","UrbanPop", "Rape","population","popover18", "percentover18")
+colnames(merged_data)=c("statename","Murder","Assault","UrbanPop", "Rape","population","popover18", "percentover18")
 merged_data
 
 library(ggplot2)  #Installing the graphics grammar library
