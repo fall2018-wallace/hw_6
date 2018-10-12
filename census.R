261dd96adc7143b9f67b974f541bf61e6a8f63cc
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 21:17:16 -0400

    Saving census.R

--- census.R
+++ census.R
@@ -7,30 +7,21 @@
 # but the comments are all original.
 
 #Reading the csv from a data frame.
-df_states <- read.csv("scprc_est_2017_18_pop_res.csv")
-df_states
+df_states=read.csv("scprc_est_2017_18_pop_res.csv")
+census=df_states
 
-new1=df_states[,-4]  #new data frame without the "STATE" column
-new1          
+census <- census[,-1:-4]
+census <- census[-c(1,53,10),]
 
-new2=new1[-53,]     #Removing the last row from the new data set
-new2
 
-new3=new2[-1,]      #Removing the first row from the new data set
-new3
-
-new4=new3[,-1:-3]  #Removing the first three columns,i.e., "SUMLEV", "REGION" and "DIVISION"
-new4 
-
-colnames(new4)=c("Statename","population","popOver18","percentOver18") #Changing the names of remaining columns
-new4
+colnames(census)=c("statename","population","popOver18","percentOver18") #Changing the names of remaining columns
+census
 
 
 #Creating a function with no parameters
 no_par=function()
 {
-  return(new4)
+  return(census)
 }
-census=no_par()
-census=census[-c(1,10),] #Removing the first and the tenth value
+no_par()
 census
